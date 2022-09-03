e2bf664b0b2d6082885ef06ceaef8b081a9088ae
rizkielviliaputri <rizkielviliaputri@gmail.com>
2022-09-03 15:22:01 +0700

    first

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- BEGIN: Footer-->
+
+<footer class="page-footer footer footer-static footer-light navbar-border navbar-shadow">
+    <div class="footer-copyright">
+        <div class="container"><span>&copy; 2022 <a href="https://akses.tubaba.go.id/login" target="_blank">PPID</a> Kab. Tulang Bawang Barat</span><span class="right hide-on-small-only">Create By : <a href="https://akses.tubaba.go.id/login">Kominfo</a></span></div>
+    </div>
+</footer>
+
+<!-- END: Footer-->
---
+++ resources/views/admin/layout/js.blade.php
@@ -0,0 +1,26 @@
+ <!-- BEGIN VENDOR JS-->
+ <script src="../../../app-assets/js/vendors.min.js"></script>
+ <!-- BEGIN VENDOR JS-->
+ <!-- BEGIN PAGE VENDOR JS-->
+ <!-- END PAGE VENDOR JS-->
+ <!-- BEGIN THEME  JS-->
+ <script src="../../../app-assets/js/plugins.js"></script>
+ <script src="../../../app-assets/js/search.js"></script>
+ <script src="../../../app-assets/js/custom/custom-script.js"></script>
+ <!-- END THEME  JS-->
+ <!-- BEGIN PAGE LEVEL JS-->
+ <script src="../../../app-assets/js/scripts/advance-ui-carousel.js"></script>
+ <!-- END PAGE LEVEL JS-->
+
+
+  <!-- END: Footer-->
+
+  <!-- BEGIN PAGE VENDOR JS-->
+    <script src="../../../app-assets/vendors/data-tables/js/jquery.dataTables.min.js"></script>
+    <script src="../../../app-assets/vendors/data-tables/extensions/responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../../../app-assets/vendors/data-tables/js/dataTables.select.min.js"></script>
+    <!-- END PAGE VENDOR JS-->
+
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="../../../app-assets/js/scripts/data-tables.js"></script>
+    <!-- END PAGE LEVEL JS-->
